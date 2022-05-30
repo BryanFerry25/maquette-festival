5beb66c96d541ccea93cc7ce4a84883f5a4aad0f
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-30 11:53:33 +0200

    creation de classe personnalisé

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,9 +1,16 @@
 <template>
 
 <Header/>
- <p class=" text-2xl">debut</p>
 
- <h1>coucou </h1>
+
+
+<div class=" flex justify-center ">
+  <img class=" lg:w-60 lg:h-auto xl:w-72 xl:h-auto" src="../../public/images/logo.svg" alt=" logo">
+</div>
+ <h2 class="  font-plus-jakarta-sans  font-semibold text-xs p-8 text-center paddingtext ">N’attendez plus et rejoignez-nous pour vivre un moment <span >palpitant </span> et <span>inoubliable</span>  </h2>
+
+<p class="paddingtext">dzbudbzeuyfezuf</p>
+
 </template>
 
 

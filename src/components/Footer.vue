a65af38594da87f72f410a8dc30888ca4a6fcc2b
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-30 22:39:57 +0200

    fin du footer ( responsive)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,8 @@
 
     <img class="w-screen max-h-24" src="../../public/images/vague_footer.webp" alt="vague footer">
     <div class=" bg-red-900">
-            <ul class=" font-plus-jakarta-sans textresponsive text-center mr-auto ml-auto space-y-10 text-white mb-16">
-                <li>Programmation</li>
+            <ul class=" font-plus-jakarta-sans textresponsive text-center mr-auto ml-auto space-y-10 text-white mb-16        grid lg:grid-cols-3 md:grid-cols-3 place-items-stretch place-self-center">
+                <li class=" mt-10">Programmation</li>
                 <li>Artistes</li>
                 <li>Le festival</li>
                 <li>Nous contacter</li>
@@ -14,9 +14,9 @@
             </ul>
 
             <div class=" flex space-x-8 justify-center pb-7 ">
-                <a class="hover:-translate-y-2 hover:rotate-[360deg] duration-300 delay-100 ease-out transition-transform  " href="https://www.instagram.com/"><img class=" w-16" src="../../public/images/Instagram.svg" alt="logo insta"></a>
-                <a href="https://www.facebook.com/"><img src="../../public/images/Twitter.svg" alt="logo twitter"></a>
-               <a href="https://www.facebook.com/"> <img src="../../public/images/Facebook.svg" alt="logo facebook"></a>
+                <a class="hover:-translate-y-2 hover:rotate-[360deg] duration-300 delay-100 ease-out transition-transform " href="https://www.instagram.com/"><img class=" w-16" src="../../public/images/Instagram.svg" alt="logo insta"></a>
+                <a class="hover:-translate-y-2 hover:rotate-[360deg] duration-300 delay-100 ease-out transition-transform "  href="https://www.facebook.com/"><img class=" w-16" src="../../public/images/Twitter.svg" alt="logo twitter"></a>
+               <a class="hover:-translate-y-2 hover:rotate-[360deg] duration-300 delay-100 ease-out transition-transform "  href="https://www.facebook.com/"> <img class=" w-16" src="../../public/images/Facebook.svg" alt="logo facebook"></a>
             </div>
     </div>
     </div>

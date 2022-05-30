9197fec66041d8a08d671ac85bfb2deeae1352b3
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-30 10:12:34 +0200

    header responsive

--- src/components/HeaderAccueil.vue
+++ src/components/HeaderAccueil.vue
@@ -3,21 +3,21 @@
 
 
 
- <div class=" flex justify-end items-center ">
-    <div class=" flex font-plus-jakarta-sans sm:text-sm gap-3 ">
+ <div class=" flex justify-end items-center pr-3 pt-5 ">
+    <div class=" flex font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 lg:p-5 xl:text-5xl xl:pr-10  ">
 
         <p>Artistes</p>
 
         <div class=" pt-1">
-        <LogoArtiste/>
+        <LogoArtiste class="md:w-5 md:h-4 lg:w-7 lg:h-6 "/>
         </div>
 
         <p class="font-plus-jakarta-sans"> |</p>
 
         <p>Programmation</p>
 
-        <div class=" pt-1">
-        <LogoCalendrier/>
+        <div class=" pt-1 ">
+        <LogoCalendrier class="md:w-5 md:h-5 lg:w-6 lg:h-6"/>
         </div>
     </div>
 </div>

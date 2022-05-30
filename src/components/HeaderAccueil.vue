0a4cba2a53dad67f53d478cca99d32ecb03de54c
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-30 11:01:31 +0200

    rajout de categorie pour le menu selon l'écran

--- src/components/HeaderAccueil.vue
+++ src/components/HeaderAccueil.vue
@@ -4,21 +4,49 @@
 
 
  <div class=" flex justify-end items-center pr-3 pt-5 pb-6 ">
-    <div class=" flex font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 lg:p-5 xl:text-5xl xl:pr-10  ">
+    <div class=" flex font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 xl:gap-4 lg:p-5 xl:text-5xl xl:pr-10  ">
 
+
+ <a class="flex font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 xl:gap-4 lg:p-5 xl:text-5xl  " href="">
         <p>Artistes</p>
 
         <div class=" pt-1">
         <LogoArtiste class="md:w-5 md:h-4 lg:w-7 lg:h-6 "/>
         </div>
+</a>
+        <p class="font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 xl:gap-4 lg:p-5 xl:text-5xl "> |</p>
 
-        <p class="font-plus-jakarta-sans"> |</p>
-
+<a class="flex font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl gap-3 xl:gap-4 lg:p-5 xl:text-5xl  "  href="">
         <p>Programmation</p>
 
         <div class=" pt-1 ">
         <LogoCalendrier class="md:w-5 md:h-5 lg:w-6 lg:h-6"/>
         </div>
+</a>
+
+
+  <p class="font-plus-jakarta-sans hidden xl:flex lg:flex xl:mt-5 lg:mt-5 "> |</p>
+<a class="font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl xl:gap-4 lg:gap-4 lg:p-5 xl:text-5xl xl:pr-10 hidden xl:flex lg:flex" href="">
+
+        <p class="hidden xl:flex lg:flex lg:text-2xl">Le festival</p>
+
+        <div class=" pt-1 hidden xl:flex lg:flex ">
+        <IconFestival class=" xl:w-6 xl:h-6 lg:w-6 lg:h-6"/>
+        </div>
+
+
+</a>
+<p class="font-plus-jakarta-sans hidden xl:flex xl:mt-5 "> |</p>
+<a class="font-plus-jakarta-sans text-sm md:text-lg lg:text-2xl xl:gap-4 lg:p-5 xl:text-5xl xl:pr-10 hidden xl:flex" href="">
+
+        <p class="hidden xl:flex">Nous contacter</p>
+
+        <div class=" pt-1 hidden xl:flex ">
+        <IconMessage class=" xl:w-6 xl:h-6"/>
+        </div>
+
+</a>
+      
     </div>
 </div>
 
@@ -31,9 +59,12 @@
 <script>
 import LogoArtiste from "./icons/LogoArtiste.vue"
 import LogoCalendrier from "./icons/LogoCalendrier.vue"
+import IconFestival from "./icons/IconFestival.vue"
+import IconMessage from "./icons/IconMessage.vue"
 
 
 export default {
-   components: { LogoArtiste, LogoCalendrier } 
+    
+   components: { LogoArtiste, LogoCalendrier, IconFestival, IconMessage } 
 }
 </script>

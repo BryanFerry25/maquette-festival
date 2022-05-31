7b90921e12769ee0e16baaa576128f4141ccd141
BryanFerry25 <bryan.ferry@edu.univ-fcomte.fr>
2022-05-31 20:05:21 +0200

    fin de la page festival

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,22 @@
 <template>
+
+
     <p>Contact</p>
+
+
+
+
 </template>
 
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import Footer from "../components/Footer.vue"
+import HeaderPage from "../components/HeaderPage.vue"
 
 
 export default {
  
-  components: {Footer }
+  components: {Footer, HeaderPage }
   
 
 };
--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -28,9 +28,70 @@ Titre="Le Festival"/>
 
 <img class=" mx-auto my-auto p-14 w-full max-w-[250px]" src="../../public/images/icondj.svg" alt="icon dj">
 
+<FlecheDown/>
+
+ <h2 class="  font-plus-jakarta-sans  font-semibold text-center textresponsive textpadding text-white ">Plus de <span >15 000 </span>visiteurs à chaque concert ! </h2>
+
+<img class=" mx-auto my-auto p-14 w-full max-w-[250px]" src="../../public/images/People.svg" alt=" personne">
+
+
+<FlecheDown/>
+
+ <h2 class="  font-plus-jakarta-sans  font-semibold text-center textresponsive textpadding text-white ">Prenez votre place pour <span >85€ </span>, et profitez également de <span>  deux consommations offertes</span> </h2>
+<img class=" mx-auto my-auto p-14 w-full max-w-[250px]" src="../../public/images/Ticket.svg" alt="ticket">
+
+<FlecheDown/>
+
+ <h2 class="  font-plus-jakarta-sans  font-semibold text-center textresponsive textpadding text-white ">Le festival peut accueillir des personnes âgées minimums de<span>  16 ans.</span> </h2>
+<img class=" mx-auto my-auto p-14 w-full max-w-[250px]" src="../../public/images/icon_16.svg" alt="age 16 ans">
+
+
+<FlecheDown/>
+
+ <h3 class="  font-plus-jakarta-sans  font-semibold text-center textresponsive textpadding text-white ">Découvrez ci-dessous quelques photos de notre festival</h3>
+
+
+<div class="  grid lg:grid-cols-3 md:grid-cols-3 place-items-stretch place-self-center ">
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img1.webp" alt=" festival image ">
+</div>
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img2.webp" alt=" festival image ">
+</div>
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img3.webp" alt=" festival image ">
+</div>
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img4.webp" alt=" festival image ">
+</div>
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img5.webp" alt=" festival image ">
+</div>
+
+<div>
+ <img class="mx-auto my-auto" src="../../public/images/festival_img6.webp" alt=" festival image ">
+</div>
+
+
 
 
 </div>
+
+
+
+
+<footer>
+  <Footer/>
+</footer>
+
+</div>
+
+
 
 
 </main>
